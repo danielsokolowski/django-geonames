5267bde0a86fab6ea0a1bfdf7bde0af14982b7f2
F. Malina <fmalina@pm.me>
2023-06-04 17:21:17 +0200

    Searchable admin that won't kill the server if you click around

--- geonames/admin.py
+++ geonames/admin.py
@@ -1,10 +1,36 @@
-from django.apps import apps
 from django.contrib import admin
-from django.contrib.admin.sites import AlreadyRegistered
+from geonames.models import (
+    GeonamesUpdate, Timezone, Language, Currency, Country,
+    Admin1Code, Admin2Code, Locality, AlternateName, Postcode
+)
 
-app_models = apps.get_app_config(__package__).get_models()
-for model in app_models:
-    try:
-        admin.site.register(model)
-    except AlreadyRegistered:
-        pass
+
+class PostcodeAdmin(admin.ModelAdmin):
+    search_fields = ['postal_code']
+
+
+class LocalityAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    raw_id_fields = ['admin1', 'admin2']
+
+
+class AlternateNameAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    raw_id_fields = ['locality']
+
+
+class Admin1CodeAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    raw_id_fields = ['locality']
+
+
+admin.site.register(GeonamesUpdate)
+admin.site.register(Timezone)
+admin.site.register(Language)
+admin.site.register(Currency)
+admin.site.register(Country)
+admin.site.register(Admin1Code)
+admin.site.register(Admin2Code)
+admin.site.register(Locality, LocalityAdmin)
+admin.site.register(AlternateName, AlternateNameAdmin)
+admin.site.register(Postcode, PostcodeAdmin)
